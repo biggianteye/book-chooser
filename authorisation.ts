74f23053adffe9846ab475cfc0b84e9f95a59831
Burhan Ali <burhan@burhanali.me.uk>
2023-07-03 21:26:42 +0100

    Run prettier

--- authorisation.ts
+++ authorisation.ts
@@ -18,47 +18,47 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
  * Reads previously authorized credentials from the save file.
  */
 async function loadSavedCredentialsIfExist(): Promise<OAuth2Client | null> {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content.toString());
-    return <OAuth2Client>google.auth.fromJSON(credentials)
-  } catch (err) {
-    return null;
-  }
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content.toString());
+        return <OAuth2Client>google.auth.fromJSON(credentials);
+    } catch (err) {
+        return null;
+    }
 }
 
 /**
  * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
  */
 async function saveCredentials(client: OAuth2Client): Promise<void> {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content.toString());
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content.toString());
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
 }
 
 /**
  * Load or request or authorization to call APIs.
  */
 async function Authorize(): Promise<OAuth2Client> {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
     return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
 }
 
 export { Authorize };
--- chooser.ts
+++ chooser.ts
@@ -1,8 +1,7 @@
-
 import { Book } from './types';
 
 export class Chooser {
     choose(books: Book[]): Book {
-        return books[Math.floor(Math.random() * books.length)]
+        return books[Math.floor(Math.random() * books.length)];
     }
 }
