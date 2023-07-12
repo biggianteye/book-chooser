a0c23c1fbb1f26e3a3771e9c5049c8607fb42b3d
Burhan Ali <burhan@burhanali.me.uk>
2023-07-12 23:28:38 +0100

    Include tag modifiers when choosing a book

--- src/types.ts
+++ src/types.ts
@@ -4,19 +4,29 @@ export class Book {
     yearPublished: number;
     goodreadsLink: string;
     goodreadsRating: number;
+    tags: Tag[];
 
     constructor(
         title: string,
         author: string,
         yearPublished: number,
         goodreadsLink: string,
-        goodreadsRating: number
+        goodreadsRating: number,
+        tags: Tag[] = []
     ) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
         this.goodreadsLink = goodreadsLink;
         this.goodreadsRating = goodreadsRating;
+        this.tags = tags;
+    }
+
+    getCalculatedRating(): number {
+        const tagSum = this.tags.reduce((result: number, tag: Tag) => {
+            return result + tag.modifier;
+        }, 0);
+        return tagSum + this.goodreadsRating;
     }
 }
 
