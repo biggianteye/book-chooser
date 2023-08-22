d4a338f570514cb4a48567cdf6070579eef1ce38
Burhan Ali <burhan@burhanali.me.uk>
2023-08-22 22:34:30 +0100

    Use command line args for exclusions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import 'dotenv/config';
 import { Chooser } from './chooser';
 import { Spreadsheet } from './spreadsheet';
+import { parse } from 'ts-command-line-args';
 
 async function main() {
+    interface cliOptions {
+        exclude?: string[];
+        help?: boolean;
+    }
+    const args = parse<cliOptions>(
+        {
+            exclude: { type: String, optional: true, multiple: true, alias: 'e', description: 'Tag to exclude' },
+            help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
+        },
+        {
+            helpArg: 'help',
+            headerContentSections: [
+                { header: 'Book Chooser', content: 'A Google Sheets-driven tool for choosing the next book to read.' },
+            ],
+        }
+    );
+
     const spreadsheet = await Spreadsheet.create(process.env.SHEET_ID);
     const books = await spreadsheet.parse();
 
-    const chooser = new Chooser();
+    const chooser = new Chooser({
+        excludeTags: args.exclude,
+    });
+
     const choice = chooser.choose(books);
 
     console.log(`${choice}`);
