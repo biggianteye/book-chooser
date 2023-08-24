544f705c1c0e217d37194f65ee0a8f94320b08d4
Burhan Ali <burhan@burhanali.me.uk>
2023-08-24 22:41:29 +0100

    Move cli config into its own file

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,10 @@ import 'dotenv/config';
 import { Chooser } from './chooser';
 import { Spreadsheet } from './spreadsheet';
 import { parse } from 'ts-command-line-args';
+import { CliOptions, usageGuideInfo } from './cli-config';
 
 async function main() {
-    interface cliOptions {
-        exclude?: string[];
-        help?: boolean;
-    }
-    const args = parse<cliOptions>(
-        {
-            exclude: { type: String, optional: true, multiple: true, alias: 'e', description: 'Tag to exclude' },
-            help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
-        },
-        {
-            helpArg: 'help',
-            headerContentSections: [
-                { header: 'Book Chooser', content: 'A Google Sheets-driven tool for choosing the next book to read.' },
-            ],
-        }
-    );
+    const args: CliOptions = parse(usageGuideInfo.arguments, usageGuideInfo.parseOptions);
 
     const spreadsheet = await Spreadsheet.create(process.env.SHEET_ID);
     const books = await spreadsheet.parse();
