80b485913ba0239dad63d7ad657806ec0895e919
Burhan Ali <burhan@burhanali.me.uk>
2026-05-22 22:36:41 +0100

    Add check for missing sheet id

--- src/export.ts
+++ src/export.ts
@@ -5,6 +5,11 @@ import { ExportBook } from './types';
 import { Spreadsheet } from './spreadsheet';
 
 async function main() {
+    if (!process.env.SHEET_ID) {
+        console.error('Missing SHEET_ID environment variable. Please set it to the ID of the Google Sheet to export from.');
+        process.exit(1);
+    }
+
     const spreadsheet = await Spreadsheet.create(process.env.SHEET_ID);
     const books = await spreadsheet.parse();
 
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import { parse } from 'ts-command-line-args';
 import { CliOptions, usageGuideInfo } from './cli-config';
 
 async function main() {
+    if (!process.env.SHEET_ID) {
+        console.error('Missing SHEET_ID environment variable. Please set it to the ID of the Google Sheet to export from.');
+        process.exit(1);
+    }
+
     const args: CliOptions = parse(usageGuideInfo.arguments, usageGuideInfo.parseOptions);
 
     const spreadsheet = await Spreadsheet.create(process.env.SHEET_ID);
